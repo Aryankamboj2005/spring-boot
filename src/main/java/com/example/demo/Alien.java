013cde4ce9eb54b65b2a3d7c54ffd4bc2b19dfda
Aryan <Aryankamboj2005@gmail.com>
2026-04-02 10:29:50 +0530

    @Qualifier('class name') use the class name but all letter are in small letter tolds the spring boot you have use the instance of which which class renmove the cimmenrrt form there

--- src/main/java/com/example/demo/Alien.java
+++ src/main/java/com/example/demo/Alien.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component // Tells Spring to manage the lifecycle of this class
@@ -8,6 +9,11 @@ public class Alien {
     
     // You MUST declare the field here so the whole class can use it
     @Autowired
+    @Qualifier("laptop") // use the class name but all letter are in small letter
+                     // tolds the spring boot you have use the instance of which
+                       // which class
+
+
     private computer comp; // as laptop is also the type of computer as laptop
                           // implements the computer
 
