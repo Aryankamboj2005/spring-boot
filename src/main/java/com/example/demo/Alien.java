a25b333a8f10ed7f50bd6d7b903f8c60d0dd98ca
Aryan <Aryankamboj2005@gmail.com>
2026-04-02 09:46:43 +0530

    Implementing Constructor Injection for Alien class to link Laptop dependency (implicitly autowired)

--- src/main/java/com/example/demo/Alien.java
+++ src/main/java/com/example/demo/Alien.java
@@ -3,14 +3,21 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component // use to tell the spring that you have to make the object of
-           // this class so that we can easily use it.
+@Component // Tells Spring to manage the lifecycle of this class
 public class Alien {
     
-    @Autowired  // Autowire the object of the Laptop class from the Spring Container
+    // You MUST declare the field here so the whole class can use it
     private Laptop lap;
 
+    // This is Constructor Injection
+    // @Autowired // In modern Spring/Spring Boot, this is OPTIONAL if you only have one constructor
+    public Alien(Laptop lap) { 
+        System.out.println("Alien Object Created via Constructor Injection!");
+        this.lap = lap;
+    }
+
     public String hlo() {
+        // Now 'lap' will work because it was initialized in the constructor
         lap.compile();
         lap.debug();
         return "Working on awesome project";
